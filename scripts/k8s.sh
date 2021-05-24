1cf6319d32ea04fc533ac7487c64d57ba6528fd2
Alexandre Cezar <alexandre_cezar@yahoo.com.br>
2021-05-24 18:54:49 -0300

    changes to the k8s script

--- scripts/k8s.sh
+++ scripts/k8s.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Connecting to the K8s cluster
-
+gcloud container clusters get-credentials microsegmentation-gke --region us-east1 --project "project name"
 
 # Configuring the namespaces
 kubectl create namespace sock-shop && \
 kubectl create namespace boutique && \
-kubectl create namespace rogue && \
-kubectl create namespace victim && \
+#kubectl create namespace rogue && \
+#kubectl create namespace victim && \
 
 # Deploying the applications
+kubectl config set-context --current --namespace=sock-shop && \
 kubectl apply -f /tmp/sock-shop.yaml && \
+kubectl config set-context --current --namespace=boutique && \
 kubectl apply -f /tmp/boutique.yaml && \
-kubectl apply -f /tmp/rogue.yaml && \
-kubectl apply -f /tmp/victim.yaml
+#kubectl config set-context --current --namespace=rogue && \
+#kubectl apply -f /tmp/rogue.yaml && \
+#kubectl config set-context --current --namespace=victim && \
+#kubectl apply -f /tmp/victim.yaml
