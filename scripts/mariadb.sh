b5f259b4607f93146950ce8fff4035449fd7a616
Alexandre Cezar <alexandre_cezar@yahoo.com.br>
2021-05-27 17:07:32 -0300

    Update mariadb.sh

--- scripts/mariadb.sh
+++ scripts/mariadb.sh
@@ -3,4 +3,5 @@
 
 # Install the MariaDB Server and create the Wordpress DB and user
 sudo apt-get update && sudo apt-get install -y mariadb-server && \
+sudo systemctl enable mariadb.service && \
 sudo mysql -h localhost -u root < /tmp/create_db.sql
