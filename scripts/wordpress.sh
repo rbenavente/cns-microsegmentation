16a3949d4522bc849c81fe1393c71b7bbeace06f
Alexandre Cezar <alexandre_cezar@yahoo.com.br>
2021-05-27 18:37:22 -0300

    Update wordpress.sh

--- scripts/wordpress.sh
+++ scripts/wordpress.sh
@@ -10,23 +10,23 @@ sudo apt-get install -y wget && sudo apt-get -y install mariadb-client && \
 #sudo apt-get update && \
 sudo add-apt-repository -y ppa:ondrej/php && \
 sudo apt-get update && \
-sudo apt-get install -y php7.0 && sudo apt-get install -y php7.0-curl && \
-sudo apt-get install -y php7.0-bcmath && sudo apt-get install -y php7.0-intl && \
-sudo apt-get install -y php7.0-gd && sudo apt-get install -y php7.0-dom && \
-sudo apt-get install -y php7.0-mcrypt && sudo apt-get install -y php7.0-iconv && \
-sudo apt-get install -y php7.0-xsl && sudo apt-get install -y && \
-sudo apt-get install -y php7.0-mbstring && sudo apt-get install -y php7.0-ctype && \
-sudo apt-get install -y php7.0-zip && sudo apt-get install -y php7.0-pdo && \
-sudo apt-get install -y php7.0-xml && sudo apt-get install -y php7.0-json && \
-sudo apt-get install -y php7.0-bz2 && sudo apt-get install -y php7.0-calendar && \
-sudo apt-get install -y php7.0-exif && sudo apt-get install -y php7.0-fileinfo && \
-sudo apt-get install -y php7.0-json && sudo apt-get install -y && \
-sudo apt-get install -y php7.0-mysqli && sudo apt-get install -y php7.0-mysql && \
-sudo apt-get install -y php7.0-posix && sudo apt-get install -y php7.0-tokenizer && \
-sudo apt-get install -y php7.0-xmlwriter && sudo apt-get install -y php7.0-xmlreader && \
-sudo apt-get install -y php7.0-phar && sudo apt-get install -y php7.0-soap && \
-sudo apt-get install -y php7.0-mysql && sudo apt-get install -y php7.0-fpm && \
-sudo apt-get install -y libapache2-mod-php7.0 && \
+sudo apt-get install -y php7.4 && sudo apt-get install -y php7.4-curl && \
+sudo apt-get install -y php7.4-bcmath && sudo apt-get install -y php7.4-intl && \
+sudo apt-get install -y php7.4-gd && sudo apt-get install -y php7.4-dom && \
+sudo apt-get install -y php7.4-mcrypt && sudo apt-get install -y php7.4-iconv && \
+sudo apt-get install -y php7.4-xsl && \
+sudo apt-get install -y php7.4-mbstring && sudo apt-get install -y php7.4-ctype && \
+sudo apt-get install -y php7.4-zip && sudo apt-get install -y php7.4-pdo && \
+sudo apt-get install -y php7.4-xml && sudo apt-get install -y php7.4-json && \
+sudo apt-get install -y php7.4-bz2 && sudo apt-get install -y php7.4-calendar && \
+sudo apt-get install -y php7.4-exif && sudo apt-get install -y php7.4-fileinfo && \
+sudo apt-get install -y php7.4-json && \
+sudo apt-get install -y php7.4-mysqli && sudo apt-get install -y php7.4-mysql && \
+sudo apt-get install -y php7.4-posix && sudo apt-get install -y php7.4-tokenizer && \
+sudo apt-get install -y php7.4-xmlwriter && sudo apt-get install -y php7.4-xmlreader && \
+sudo apt-get install -y php7.4-phar && sudo apt-get install -y php7.4-soap && \
+sudo apt-get install -y php7.4-mysql && sudo apt-get install -y php7.4-fpm && \
+sudo apt-get install -y libapache2-mod-php7.4 && \
 sudo sed -i -e"s/^memory_limit\s*=\s*128M/memory_limit = 512M/" /etc/php/7.0/apache2/php.ini && \
 sudo a2enmod rewrite && \
 sudo a2enmod headers && \
